9344d8f387e29866f51acbf90beebc5c697ddd21
Edgar <34298>
2021-09-24 09:31:23 +0200

    new file:   ditbenik.py 	new file:   play_with_python.py 	new file:   poem_printing.py 	modified:   HelloWorld.py 	modified:   hello you!.py 	modified:   poem_printing.py

---
+++ poem_printing.py
@@ -0,0 +1,7 @@
+vers1 = " mijn naam is edgar en ik woon in zandvoort.\n"
+vers2 = "hier waar ik vandaan kom is het elk weekend raak als je de formule 1 aanhoort.\n"
+vers3 = "hier vers 3\n"
+vers4 = "hier vers 4\n"
+
+
+print(vers1,vers2, vers3)
