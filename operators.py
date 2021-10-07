4ed1bcca3b1789bd4cda16acb03ffc0d84fc70c0
EdRma <34298@ma-web.nl>
2021-10-07 11:17:23 +0200

    test

--- operators.py
+++ operators.py
@@ -1,6 +1,18 @@
-x = 5
+x = 12
 y = 3
-answer = input("welke operator?")
+answer = input("whitch operator?")
 print("")
-if answer.lower() == "plus":
+if answer.lower() == "	Addition":
  print(x + y)
+elif answer.lower() =="	Subtraction":
+ print(x - y)
+elif answer.lower() =="	Multiplication":
+ print(x * y)
+elif answer.lower() =="	Division":
+ print(x / y)
+elif answer.lower() =="	Modulus":
+ print(x % y)
+elif answer.lower() =="Exponentiation":
+ print(x ** y)
+elif answer.lower() =="	Floor division":
+ print(x // y)
---
+++ stutter simulator.py
@@ -0,0 +1,13 @@
+print("di... di...dit is de st... st... stotter si... si... simulator")
+print("ty... ty... type hi... hi... hier wat je wi... wi... wilt la... la... laten st... st... stotteren")
+running = True
+while running == True:
+    antwoord = input("vu...vul in...: ")
+    print("")
+    userlist = antwoord.split()
+    for x in userlist:
+        if len(x) > 2:
+            print(x[0:2]+"...", x[0:2]+"..." ,x)
+        else:
+            print(x)
+            
