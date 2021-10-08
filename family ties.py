7fbd9aac110d99d54a199daf8b382767ade8728e
EdRma <34298@ma-web.nl>
2021-10-08 10:56:39 +0200

    ook af

---
+++ family ties.py
@@ -0,0 +1,8 @@
+naam = input("je naam? ")
+leeftijd = input("hoe oud ben je? ")
+woonplaats = input("waar woon je? ")
+afstand = input("hoe ver is dat van school? ")
+zelf = input("vul wat van jezelf in, dus het begint met ik: ")
+print()
+print("hallo mijn naam is "+ naam + " en ik ben " + leeftijd + " jaar oud. ik woon in " + woonplaats + " en dat is " + afstand + "km van school vandaan.")
+print("ik "+zelf)
