e5e3859b574c3f507a3b72da5a61a220b86e917e
EdRma <34298@ma-web.nl>
2021-10-08 10:43:54 +0200

    ook af

---
+++ stel_vragen.py
@@ -0,0 +1,7 @@
+antwoord0 = input("Hoe oud ben je? ")
+antwoord1 = input("waar woon je? ")
+antwoord2 = input("wat is je favoriete eten? ")
+
+print("Aha, je bent dus " + antwoord0 + " jaar oud, leuk zeg!")
+print("en je woont dus in " + antwoord1 + ", klinkt leuk!")
+print("en je favoriete eten is " + antwoord2 + ", lijkt me lekker...")
