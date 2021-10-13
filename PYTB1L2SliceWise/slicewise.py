e2889d58cbd1019f0a1113af8be64301fc91aad8
EdRma <34298@ma-web.nl>
2021-10-13 13:01:19 +0200

    modified:   PYTB1L2SliceWise/slicewise.py

---
+++ PYTB1L2SliceWise/slicewise.py
@@ -0,0 +1,21 @@
+tekst1 = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+voornaam1 = tekst1[37:41]
+voornaam2 = tekst1[43:48]
+voornaam3 = tekst1[61:66]
+print(voornaam1,voornaam2,voornaam3)
+
+tekst2 = 'SD vakken zijn bijvoorbeeld Python, UXD, Frontend development en Backend development. Wat leuk!'
+vak1 = tekst2[28:34]
+vak2 = tekst2[36:39]
+vak3 = tekst2[41:61]
+vak4 = tekst2[65:85]
+print(vak1,vak2,vak3,vak4)
+
+tekst3 = 'Wat is hier het laatste woord?'
+laatstewoord = tekst3[24:30]
+print(laatstewoord)
+
+tekst4 = 'Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee'
+woord1 = tekst4[4:7]
+woord2 = tekst4[28:32]
+print(woord1,woord2)
